1b24c9bfd2027d8b25eecfe336c281e02ea07c3f
gregnagle <gregojanagle@gmail.com>
2016-04-30 02:08:50 -0400

    basic landing layout

--- js/main.js
+++ js/main.js
@@ -1,3 +1,22 @@
 $(function(){
-  
+
+/////////////////////////////////////
+// window.load
+////////////////////////////////////
+  var $everything = $('#everything');
+
+  $(window).load(function(){
+    // setTimeout(function(){
+      $everything.addClass('loaded');
+    // }, 3000);
+  });
+
+
+  $('.main-menu-trigger').mouseenter(function(){
+    $('.main-line').addClass('line-trigger');
+  });
+  $('.main-menu-trigger').mouseleave(function(){
+    $('.main-line').removeClass('line-trigger');
+  });
+
 });
