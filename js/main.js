7ee18e8a591d281a8e8627aa73040daa4ad0687e
gregnagle <gregojanagle@gmail.com>
2016-04-30 03:59:46 -0400

    made the menu function

--- js/main.js
+++ js/main.js
@@ -12,11 +12,81 @@ $(function(){
   });
 
 
-  $('.main-menu-trigger').mouseenter(function(){
+
+/////////////////////////////////////
+// Menu interations and animations
+/////////////////////////////////////
+
+  var $mainMenuClickFlag = false,
+      $mainMenuFlag = false;
+
+  // set .menu-trigger click flag
+  function $clickFlagSettr() {
+    $mainMenuClickFlag = true;
+    console.log($mainMenuClickFlag);
+    setTimeout(function(){
+      $mainMenuClickFlag = false;
+      console.log($mainMenuClickFlag);
+    }, 300);
+  }
+
+  // click actions
+  function $mainMenuFlagged() {
+    if( !$mainMenuFlag ) {
+      $mainMenuFlag = true;
+      $('.main-menu').addClass('main-menu-active');
+    }
+    else {
+      $mainMenuFlag = false;
+      $('.main-menu').removeClass('main-menu-active');
+    }
+  }
+
+  // if .main-menu-trigger is flagged
+  function $mainMenuClick() {
+    if( !$mainMenuClickFlag ) {
+      $clickFlagSettr();
+      $mainMenuFlagged();
+    }
+  }
+
+  // actual click
+  $('.main-menu-trigger').click( $mainMenuClick );
+
+
+  /////////////////////////////////////
+  // hover events
+
+  // add classes
+  function mainAddr(){
+    $('.main-menu-trigger').addClass('triggered');
     $('.main-line').addClass('line-trigger');
-  });
-  $('.main-menu-trigger').mouseleave(function(){
+  }
+
+  // removes classes
+  function $mainRemovr() {
+    $('.main-menu-trigger').removeClass('triggered');
     $('.main-line').removeClass('line-trigger');
-  });
+  }
+
+  // if main-menu is not flagged
+  // else main-menu is flagged
+  function mainMenuIN() {
+    if ( !$mainMenuFlag ) {
+      mainAddr();
+    } else {
+      $mainRemovr();
+    }
+  }
+  function mainMenuOUT() {
+    if ( !$mainMenuFlag ) {
+      $mainRemovr();
+    } else {
+        mainAddr();
+    }
+  }
+
+  // main-menu-trigger hover events
+  $('.main-menu-trigger').hover(mainMenuIN, mainMenuOUT);
 
 });
